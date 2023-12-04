ac1e9af69e8a8324ed59d3d61c7a2c62efb2e72b
sigmapng <riabkoyuliia@gmail.com>
2023-12-04 03:01:56 +0200

    3rd task

--- lab-3.cpp
+++ lab-3.cpp
@@ -3,24 +3,67 @@
 using namespace std;
 
 // task 1
-void firstTask() {
-	double a;
-	checkpoint:
-	cout << "Enter your desirable 'a' value, but it must be  " << endl;
+void getSumOfDigits() {
+	int a;
+
+	Skip:
+	cout << "Hi! Enter your desirable 'a' value, it must be a 3-digit integer: " << endl;
 	cin >> a;
 
-	if (a == 1) {
+	int originalInput = a;
 
+	int sum = 0;
+	while (a > 0) {
+		int digit = a % 10;
+		if (digit > 3) {
+			sum += digit;
+		}
+		a /= 10;
 	}
+
+	if (originalInput < 100 || originalInput > 999) {
+		cerr << "Invalid input. Please enter a three-digit positive number." << endl;
+		goto Skip;
+	}
+
+	cout << "Sum of digits greater than 3: " << sum << endl;
 }
 
 // task 2
-/*void secondTask() {
-	double a;
-}*/
+void getScheduleForADay() {
+	int a;
+	cout << "Hi! Wanna see your schedule? Enter day number you would like to see" << endl;
+	cin >> a;
+
+	switch (a) {
+		case 1:
+			cout << "4. Cross-platform programming" << endl << "5. Philosophy" << endl;
+			break;
+		case 2:
+			cout << "2. Foreign Language" << endl << "3. Foreign Language" << endl;
+			break;
+		case 3:
+			cout << "1. Maths" << endl << "2. Foreign Language" << endl << "3. Applied algorithms and data structures"
+				 << endl << "5. C++ Programming" << endl;
+			break;
+		case 4:
+			cout << "2. Converged network infrastructure" << endl;
+			break;
+		case 5:
+			cout << "3. Cross-platform programming" << endl;
+			break;
+		case 6 :
+		case 7 :
+			cout << "Grab a cup of tea, it's a weekend" << endl;
+			break;
+
+		default:
+			cout << "Enter a correct value please!" << endl;
+	}
+}
 
 int main() {
-	firstTask();
-//	secondTask();
+	getSumOfDigits();
+	getScheduleForADay();
 	return 0;
 }
