dc36a0d4fd4a40f365d56fc08edbc22a17255b6b
sigmapng <riabkoyuliia@gmail.com>
2024-06-07 08:54:42 +0300

    prime number validator

--- cpp-duikt-2nd-sem/lab-4.cpp
+++ cpp-duikt-2nd-sem/lab-4.cpp
@@ -1,3 +1,35 @@
-//
-// Created by yuliia on 02.05.2024.
-//
+#include <iostream>
+
+using namespace std;
+
+bool isPrime(int num);
+
+int main() {
+	cout << "- - - - -" << endl;
+	int number;
+	cout << "Enter a number " << endl;
+	cin >> number;
+
+	if (isPrime(number)) {
+		cout << number << " is a prime number." << endl;
+	} else {
+		cout << number << " is not a prime number." << endl;
+	}
+	cout << "- - - - -" << endl;
+
+	return 0;
+}
+
+bool isPrime(int num) {
+	if (num <= 1) {
+		return false;
+	}
+
+	for (int i = 2; i * i <= num; ++i) {
+		if (num % i == 0) {
+			return false;
+		}
+	}
+
+	return true;
+}
